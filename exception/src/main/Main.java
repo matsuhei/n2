7e3d9b3518054109933107a967e9754aebad0a02
matsuhei <matsumoto_ryohei@cam-inc.co.jp>
2021-03-15 09:55:42 +0900

    fix readme & code

--- exception/src/main/Main.java
+++ exception/src/main/Main.java
@@ -6,6 +6,7 @@ import main.helper.StringHelper;
 public class Main {
     /**
      * 全体をキャッチ
+     *
      * @return {String}
      */
     public static String name() {
@@ -20,6 +21,7 @@ public class Main {
     /**
      * 絶対ダメなやつ(コメントありでも基本的に非推奨)
      * Exceptionを握りつぶしているだけでほかは何もしていない
+     *
      * @return {String}
      */
 
@@ -33,7 +35,7 @@ public class Main {
 
     /**
      * 料金の支払い これはプログラム上のエラーではなく、リクエストによるエラーのためException
-     * 
+     *
      * @param price
      * @param balance
      * @throws TestException
@@ -45,13 +47,14 @@ public class Main {
     }
 
     /**
-     * 配列を生成するもの(RunTimeExceptionのケース プログラム上で異常系の処理が起こったときのものを確認するため)
+     * 配列を生成するもの(RunTimeExceptionのケースの確認 プログラム上で異常系の処理が起こったときのものを確認するため)
+     *
      * @param size
      * @return
      */
     public static String[] gen(Integer size) {
         String[] result = new String[256];
-        for (Integer i = 0; i < size ; i++ ){
+        for (Integer i = 0; i < size; i++) {
             result[i] = i.toString();
         }
         return result;
