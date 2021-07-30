9119fc36a347d546a768de9bbe18761961d5e8c7
matsuhei <mat0315ryo@gmail.com>
2021-07-31 00:40:28 +0900

    coin処理完了

--- golang/myapp/atcoder/coin/coins.go
+++ golang/myapp/atcoder/coin/coins.go
@@ -11,4 +11,72 @@ func main() {
 	fmt.Scanf("%d", &c)
 	fmt.Scanf("%d", &x)
 
+	var result int
+
+	if c > 0 {
+		for i := 1; i <= c; i++ {
+			c2 := i * 50
+
+			if x == c2 {
+				result++
+			}
+
+			if b > 0 {
+				for j := 1; j <= b; j++ {
+					b2 := j * 100
+					if x == b2+c2 {
+						result++
+					}
+
+					if a > 0 {
+						for k := 1; k <= a; k++ {
+							a2 := k * 500
+							if x == a2+b2+c2 {
+								result++
+							}
+						}
+					}
+				}
+			}
+
+			if a > 0 {
+				for k := 1; k <= a; k++ {
+					a2 := k * 500
+					if x == a2+c2 {
+						result++
+					}
+				}
+			}
+
+		}
+	}
+
+	if b > 0 {
+		for j := 1; j <= b; j++ {
+			b2 := j * 100
+
+			if x == b2 {
+				result++
+			}
+
+			if a > 0 {
+				for k := 1; k <= a; k++ {
+					a2 := k * 500
+					if x == a2+b2 {
+						result++
+					}
+				}
+			}
+		}
+	}
+
+	if a > 0 {
+		for j := 1; j <= a; j++ {
+			a2 := j * 500
+			if x == a2 {
+				result++
+			}
+		}
+	}
+	fmt.Println(result)
 }
